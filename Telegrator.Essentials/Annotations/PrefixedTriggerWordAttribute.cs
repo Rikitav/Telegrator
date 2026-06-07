19b1fc43f8aebbc0f8772b223af95cee9bc044c6
gutii <gutii@RIKITAV>
2026-06-08 02:14:18 +0400

    * Added Telegrator.Essentials package

---
+++ Telegrator.Essentials/Annotations/PrefixedTriggerWordAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Telegram.Bot.Types;
+using Telegrator.Attributes;
+using Telegrator.Core.Filters;
+
+namespace Telegrator.Annotations;
+
+public class PrefixedTriggerWordAttribute : FilterAnnotation<Message>
+{
+    public string[] Prefixes { get; set; } = ["/"];
+
+    public override bool CanPass(FilterExecutionContext<Message> context)
+    {
+        if (context.Input.Text is { Length: > 0 })
+        {
+            string text = context.Input.Text;
+            return Prefixes.Any(p => text.StartsWith(p));
+        }
+
+        return false;
+    }
+}
